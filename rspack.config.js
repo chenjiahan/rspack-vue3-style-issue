8e818063b86eb509c1625a462d90848b230a04f7
chenjiahan <chenjiahan.jait@bytedance.com>
2023-07-19 11:20:42 +0800

    chore: init

---
+++ rspack.config.js
@@ -0,0 +1,45 @@
+const { VueLoaderPlugin } = require("vue-loader");
+
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
+	context: __dirname,
+	entry: {
+		main: "./src/main.js"
+	},
+	builtins: {
+		html: [
+			{
+				template: "./index.html"
+			}
+		],
+		define: {
+			__VUE_OPTIONS_API__: JSON.stringify(true),
+			__VUE_PROD_DEVTOOLS__: JSON.stringify(false)
+		}
+	},
+	devServer: {
+		historyApiFallback: true
+	},
+	plugins: [new VueLoaderPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: "vue-loader",
+				options: {
+					experimentalInlineMatchResource: true
+				}
+			},
+			{
+				test: /\.less$/,
+				loader: "less-loader",
+				type: "css"
+			},
+			{
+				test: /\.svg$/,
+				type: "asset/resource"
+			}
+		]
+	}
+};
+module.exports = config;
